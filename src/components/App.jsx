7b1eb0304f645a3409928bf5d0c7d87f34509e34
Seweryn <sewcio0212@gmail.com>
2024-04-25 19:16:39 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,6 @@
 import React from 'react';
 import Profile from './Profile';
-
-const userData = {
-  username: 'Jacques Gluke',
-  tag: 'jgluke',
-  location: 'Ocho Rios, Jamaica',
-  avatar: 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png',
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308,
-  },
-};
+import userData from './user.json';
 
 const App = () => {
   return (

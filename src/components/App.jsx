a4f58c8fcc4927f7d2e635a78362488c9ede71aa
Seweryn <sewcio0212@gmail.com>
2024-04-29 11:45:45 +0200

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+// Components
 import Profile from './Profile';
 import Statistics from './Statistics';
+import FriendList from './FriendsList';
+// Data
 import userData from './user.json';
 import stats from './data.json';
+import friends from './friends.json';
 
 const App = () => {
   return (
@@ -18,6 +22,11 @@ const App = () => {
         id={stats.id}
         label={stats.label}
         percentage={stats.percentage}
+      />
+      <FriendList
+        avatar={friends.avatar}
+        label={friends.label}
+        percentage={friends.percentage}
       />
     </div>
   );
